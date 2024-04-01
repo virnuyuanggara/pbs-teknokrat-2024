d47110988791230c744c13a760b4ec07629835c3
prstya1 <121971128+prstya1@users.noreply.github.com>
2024-04-01 21:35:17 +0700

    add new konfig vercel.json

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ const db = mysql.createConnection({
   host: 'localhost', // Host database
   user: 'root', // Pengguna database
   password: '', // Kata sandi database
-  database: 'reser_hotel' // Mengasumsikan 'mahasiswa' adalah nama database Anda
+  database: 'mahasiswa' // Mengasumsikan 'mahasiswa' adalah nama database Anda
 });
 
 // Mengekspor variabel koneksi database untuk digunakan di modul lain
